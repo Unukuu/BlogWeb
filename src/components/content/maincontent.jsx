aa8a17d6dd99f21a5ebbe661cf2ba115113b6b78
unukuu <ll040299@gmail.com>
2024-08-15 12:24:25 +0800

    slide

--- src/components/content/maincontent.jsx
+++ src/components/content/maincontent.jsx
@@ -3,40 +3,52 @@ import { GrPrevious } from "react-icons/gr";
 import Label from "./label";
 import { useEffect, useState } from "react";
 const Maincontent = () => {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
   const [maincontentData, setMaincontentData] = useState([]);
   const getMaincontentdata = async () => {
-    const respo = await fetch(
-      `https://dev.to/api/articles?page=${count}&per_page=1`
-    );
+    const respo = await fetch(`https://dev.to/api/articles?page=1&per_page=5`);
     const data = await respo.json();
-    setMaincontentData(data[0]);
+    setMaincontentData(data);
   };
   useEffect(() => {
     getMaincontentdata();
-  }, [count]);
+  }, []);
   return (
     <>
       <div
-        style={{
-          background: `url(${maincontentData?.social_image})`,
-        }}
-        className={`container m-auto h-[651px] bg-[rgba(0,0,0,0.4)] bg-blend-darken rounded-xl bg-cover bg-center p-3 flex items-end`}
+        className="overflow-hidden
+       flex container m-auto rounded-xl shadow-2xl"
       >
-        <div className="h-[50%] w-[50%] bg-white rounded-xl p-10 border">
-          <Label text={maincontentData?.user?.username} />
-          <p className="text-[38px] font-bold">{maincontentData?.title}</p>
-          <p className="text-[#97989F]">
-            {maincontentData?.readable_publish_date}
-          </p>
-        </div>
+        {maincontentData?.map((data) => {
+          return (
+            <div
+              className={`transition-all duration-500 ease-in-out translate-x-[-${count}00%]`}
+            >
+              <div
+                style={{
+                  background: `url(${data?.social_image})`,
+                }}
+                className={`container m-auto h-[651px] bg-[rgba(0,0,0,0.4)] w-[1600px] bg-blend-darken bg-cover bg-center p-3 flex items-end`}
+              >
+                <div className="shadow-2xl h-[50%] w-[50%] bg-white rounded-xl p-10 border">
+                  <Label text={data?.user?.username} />
+                  <p className="text-[38px] font-bold">{data?.title}</p>
+                  <p className="text-[#97989F]">
+                    {data?.readable_publish_date}
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
+
       <div className="flex gap-2 container m-auto justify-end mt-3">
         <button
           onClick={() => {
             setCount(count - 1);
-            if (count <= 1) {
-              setCount(30);
+            if (count <= 0) {
+              setCount(4);
             }
           }}
           className="border border-black p-4 rounded"
@@ -46,6 +58,9 @@ const Maincontent = () => {
         <button
           onClick={() => {
             setCount(count + 1);
+            if (count >= 4) {
+              setCount(0);
+            }
           }}
           className="border border-black p-4 rounded"
         >
